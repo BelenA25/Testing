918f9646131ffb7f7eb3ef72da5f671778277712
Belen <beckiar23@gmail.com>
2024-10-08 08:50:55 -0400

    Prueba

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Auth from './Auth';
 const App = () => {
   return (
     <div>
-      <h1>Firebase Authentication with Vite</h1>
+      <h1>Firebase Authentication with Vite probando</h1>
       <Auth />
     </div>
   );
